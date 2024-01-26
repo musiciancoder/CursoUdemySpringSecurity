fb7d441a9a04703d68f9b73af4d194ff01c49aa1
musiciancoder <andresguitarra1980@gmail.com>
2024-01-26 11:21:24 -0300

    Spring security in action

---
+++ src/main/java/com/example/first_app/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.first_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Spring security rocks!";
+    }
+
+}
